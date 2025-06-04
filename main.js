c126d6c7fd132ee223b14e65b23c0ef72bea2669
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2025-06-04 21:59:28 +0530

    adjust dimensions of canvas

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ const log = console.log;
 
 log("main");
 
+const mainContainer = document.querySelector(".main-container");
+const canvas = document.querySelector('.my-canvas');
+
+const ctx = canvas.getContext('2d');
+
+const mcHeight = mainContainer.clientHeight;
+const mcWidth = mainContainer.clientWidth;
+
+log(mcHeight);
+log(mcWidth);
+
+canvas.height = 9/10 * mcHeight;
+canvas.width = 7/10 * mcWidth;
+
+log(canvas.height);
+log(canvas.width);
